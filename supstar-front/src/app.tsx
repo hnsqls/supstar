48f2df0ef33c2d56a20f28cd15a4de038dc3e113
hnsqls <2661108038@qq.com>
2025-03-10 15:13:50 +0800

    add：前端携带session

--- supstar-front/src/app.tsx
+++ supstar-front/src/app.tsx
@@ -132,5 +132,6 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  */
 export const request = {
   baseURL: "http://localhost:8111",
+  withCredentials:true,
   ...errorConfig,
 };
